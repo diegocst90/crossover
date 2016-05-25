0f66ca75faab1a9c6c5a6cb34827bfa4ad6d77fb
Diego R Castillo Rios <diego.cst90@gmail.com>
2016-05-25 17:33:30 -0500

    Added base item

--- app/components/items/_item.js
+++ app/components/items/_item.js
@@ -1,25 +1,51 @@
 'use strict';
+'use strict';
 
 /*
- * components/items/_item.js
- * Class definition for item
+ * models/object.js
  *
- * (c) 2016 Diego Castillo
+ * (c) 2015 diegocst90
  * License: MIT
  */
 
-function Item(data) {
-    data = data || {};
+angular.module('components.items')
+.factory('ItemObject',['$q', '$state',
+    function($q, $state){
+        var item = function (data) {
+            this.data = angular.extend({
+                type: '',
+                code: '',
+                owner: '',
+                time_started: Number(new Date()),
+                result_status: 0, //Pending
+                measures: {
+                    metrics: { progress: 0, failed: false },
+                    build: { progress: 0, failed: false },
+                    unit_test: { progress: 0, failed: false },
+                    functional_test: { progress: 0, failed: false }
+                },
+                expanded: false
+            },data);
+        };
 
-    /*Object.reverseMerge(data, {
-        name: "McLovin",
-        age: 25,
-        homeState: "Hawaii"
-    }); use angular.extend */
+        item.prototype.isBuild = function() {
+            return (this.data.type == 'build');
+        };
 
-    return {
-        introduce: function() {
-            return "Hi I'm " + I.name + " and I'm " + I.age;
-        }
-    };
-}
+        item.prototype.isFirewall = function() {
+            return (this.data.type == 'firewall');
+        };
+
+        item.prototype.getItemPicture = function() {
+            return (this.isBuild())? 'computer.jpg' : 'firewall.jpg';
+        };
+
+        item.prototype.getResultState = function() {
+            if (this.data.result_status == 0) return 'Pending';
+            if (this.data.result_status == 1) return 'Running';
+            if (this.data.result_status == 2) return (this.isBuild())? 'Completed' : 'Accepted'; //Completed or Accepted
+            if (this.data.result_status == 3) return (this.isBuild())? 'Fail' : 'Rejected'; //Fail or Rejected
+        };
+
+        return item;
+    }]);
--- app/modules/home/home_ctrl.js
+++ app/modules/home/home_ctrl.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('crossover.home', ['ui.router'])
-.controller('HomeCtrl', ['$scope','ItemsCollection', function($scope, ItemsCollection) {
+    .controller('HomeCtrl', ['$scope','ItemsCollection','ItemObject', function($scope, ItemsCollection, ItemObject) {
+        ItemsCollection.setItems([]);
 
-}]);
+        var build1 = new ItemObject({
+            code: 'ABC',
+            owner: 'xxx'
+        });
+
+        ItemsCollection.addItem(build1);
+
+        $scope.items_collection = ItemsCollection.getItems();
+    }]);
