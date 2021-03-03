8a7dd01ee0534ec7ae4743658721344acf823983
Diego Castillo <dcastillo@assanet.com>
2021-03-02 21:00:14 -0500

    Added comment

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task('git-check', function(done) {
   if (!sh.which('git')) {
     console.log(
       '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required.',
+      '\n  Git yes, the version control system, is required.',
       '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
       '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
     );
